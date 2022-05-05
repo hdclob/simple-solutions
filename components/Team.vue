18307871126acdb59fbc5c8251557dfdb1cb586a
Hugo Braga <hugo.braga@qavalue.com>
2022-05-05 15:34:48 +0100

    new images

--- components/Team.vue
+++ components/Team.vue
@@ -17,37 +17,37 @@
           {
             id: 1,
             name: 'Hugo Braga',
-            image: require('@/assets/images/team/default-user-image.png'),
+            image: require('@/assets/images/team/hugo.png'),
             role: 'Web Developer'
           },
           {
             id: 2,
             name: 'Jack Moorhouse',
-            image: require('@/assets/images/team/default-user-image.png'),
+            image: require('@/assets/images/team/jack.png'),
             role: 'Lead Designer'
           },
           {
             id: 3,
             name: 'Markus Simpson',
-            image: require('@/assets/images/team/default-user-image.png'),
+            image: require('@/assets/images/team/markus.png'),
             role: 'Web Developer'
           },
           {
             id: 4,
             name: 'Joshua Emmanuel',
-            image: require('@/assets/images/team/default-user-image.png'),
+            image: require('@/assets/images/team/joshua.png'),
             role: 'Sound Engineer'
           },
           {
             id: 5,
             name: 'Filipe Cavaleiro',
-            image: require('@/assets/images/team/default-user-image.png'),
+            image: require('@/assets/images/team/filipe-charis.png'),
             role: 'Game Developer'
           },
           {
             id: 6,
             name: 'Charis Christodoulakis',
-            image: require('@/assets/images/team/default-user-image.png'),
+            image: require('@/assets/images/team/filipe-charis.png'),
             role: 'Game Developer'
           },
         ]
